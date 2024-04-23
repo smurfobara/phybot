df1ebd7eea68c0affdb17b1f6ed83d81162828d3
smurfobara <167231428+smurfobara@users.noreply.github.com>
2024-04-23 20:34:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from telebot import types
 import textes
 import time
 
-VERSION = "0.3 Alpha"
+VERSION = "0.5 Beta"
+
+
+listMessages = []
 
 txt = open("users.txt")
 contenttext = txt.read()
@@ -23,12 +26,14 @@ def start(message):
     s.close()
     k.close()
     markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton("Силы с определениями и формулами", callback_data="listOfPowers")
+    btn1 = types.InlineKeyboardButton("Список сил", callback_data="listOfPowers")
     btn2 = types.InlineKeyboardButton("[НОВОЕ] Поиск", callback_data="find")
-    markup.row(btn1, btn2)
     btn3 = types.InlineKeyboardButton("Калькулятор давления, плотности и силы", callback_data="calculator")
-    markup.row(btn3)
     btn4 = types.InlineKeyboardButton("О разработке бота", callback_data="development")
+    btn5 = types.InlineKeyboardButton("Список определений", callback_data="listOfOprs")
+    markup.row(btn1, btn5)
+    markup.row(btn2)
+    markup.row(btn3)
     markup.row(btn4)
     bot.send_message(message.chat.id, "Привет!🙌\n Этот бот поможет тебе с физикой! Выбери что тебе нужно 👇👇", reply_markup=markup)
     
@@ -48,38 +53,56 @@ def massa(message):
 def finding(message):
     text = str(message.text)
     text = text.lower()
-    textmass = text.split()
-    del textmass[0]
-    if len(textmass) == 0:
+    lines = text.split()
+    listPowers = textes.listPowers
+    listOprs = textes.listOprs
+    isfinded = None
+    del lines[0]
+    if len(lines) == 0:
         bot.send_message(message.chat.id, "⚠️ Укажите текст который пытаетесь найти /find [текст]")
-    for word in textmass:
-        if word in textes.power:
-            bot.send_message(message.chat.id, textes.power)
-        elif word in textes.powerEarth:
-            bot.send_message(message.chat.id, textes.powerEarth)
-        elif word in textes.powerVes:
-            bot.send_message(message.chat.id, textes.powerVes)
-        elif word in textes.PowerRavno:
-            bot.send_message(message.chat.id, textes.PowerRavno)
-        elif word in textes.powerTr:
-            bot.send_message(message.chat.id, textes.powerTr)
-        elif word in textes.powerUpr:
-            bot.send_message(message.chat.id, textes.powerUpr)
+    else:
+        for word in lines:
+            print("word in lines")
+            for line in listPowers:
+                print("line in textes")
+                if word in line:
+                    print("word in line")
+                    bot.send_message(message.chat.id, line)
+                    isfinded = True
+                    print (isfinded)
+                    break
+        for word in lines:
+            for line in listOprs:
+                if word in line:
+                    bot.send_message(message.chat.id, line)
+                    break
         
 
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
+    listOprs = textes.listOprs
+    listPowers = textes.listPowers
     if callback.data == "listOfPowers":
-        bot.send_message(callback.message.chat.id, "Список сил 👇👇👇")
-        bot.send_message(callback.message.chat.id, textes.power + textes.powerTr + textes.powerEarth + textes.powerUpr + textes.powerVes + textes.PowerRavno)
+        bot.send_message(callback.message.chat.id, "Список сил (пока не полный)👇👇👇")
+        for index in listPowers:
+            bot.send_message(callback.message.chat.id, index)
+        bot.send_message(callback.message.chat.id, "Чтобы вернуться в меню нажмите здесь👉 /start")
     elif callback.data == "find":
         bot.send_message(callback.message.chat.id, "Новая функция: поиск! Пока может работать не очень стабильно, но в будущем все исправится! \n Для использования введите /find и текст, который пытаетесь найти.")
     elif callback.data == "calculator":
         bot.send_message(callback.message.chat.id, 'Калькулятор пока не доступен, как говорится "Coming soon..."')
     elif callback.data == "development":
         bot.send_message(callback.message.chat.id, f"Развиваю бот в одиночку, по мере сил и возможностей. Версия на данный момент: {VERSION} \n Предложить идею: @smurfobara")
-        bot.send_message(callback.message.chat.id, f"Список изменений на версию {VERSION}: \n ПОИСК - первая версия поиска! \n Доработка и оптимизация кода")
+        bot.send_message(callback.message.chat.id, f"Список изменений на версию {VERSION}: \n Переход в Beta \n Добавлен раздел 'определения' \n Полностью переработана и улучшена система поиска \n Доработка и оптимизация кода")
+    elif callback.data == "listOfOprs":
+        bot.send_message(callback.message.chat.id, "Список определений (пока не полный)👇👇👇")
+        for index in listOprs:
+            bot.send_message(callback.message.chat.id, index)
+        bot.send_message(callback.message.chat.id, "Чтобы вернуться в меню нажмите здесь👉 /start")
+        
+
+
 
 @bot.message_handler(commands=["botclosing"])
 def closing(message):
